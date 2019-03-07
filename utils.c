253092039bd175d0bc8f22666758598a57781fd7
Yoann RIBEIRO <yoribeir@e2r9p7.42.fr>
2019-03-07 14:49:10 +0100

    %#.x %#.0x fixed

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: yoribeir <yoribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 12:47:12 by yoann             #+#    #+#             */
-/*   Updated: 2019/03/07 14:20:57 by yoribeir         ###   ########.fr       */
+/*   Updated: 2019/03/07 14:48:21 by yoribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,7 +105,7 @@ char                *itoa_base_long(t_parser *p, long long int n, int base, char
 	}
 	str[nb_len1(n)] = '\0';
 	i = 0;
-	if (p->f & ZEROVALUE && ((p->f & PRECISION) && !p->precision) && !(p->f & PREFIX))
+	if (p->f & ZEROVALUE && ((p->f & PRECISION) && !p->precision) && (!(p->f & PREFIX) || p->format == 'x'))
 		return ("");
 	while (nb)
 	{
